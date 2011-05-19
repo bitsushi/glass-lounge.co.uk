46992f5f59ed05bc7657922970b1df12bc06c379
John Rees <john@bitsushi.com>
2011-05-19 16:28:49 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glasslounge::Application.config.secret_token = '98fe6dd77e48b2edc22dcb11a0b4dabad51e2938bed4d1086e22392e09cb79ae31a51607d35790bc37e2d0abef9bab0a5d366724235ffc81ad9e3eea6841af17'
